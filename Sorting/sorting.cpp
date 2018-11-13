0f2dbd33f9786c91b1936a1ea0537e06603b1342
Robert Usey <ruusey@gmail.com>
2018-11-13 13:16:31 -0500

    Final

--- Sorting/sorting.cpp
+++ Sorting/sorting.cpp
@@ -1,361 +1,319 @@
 //============================================================================
-// Name        : Sort3.cpp
+// Name        : sorting.cpp
 // Author      : Robert Usey
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C, Ansi-style
+// Assignment  : CSCE350 Project3
+// Copyright   : Copyright Robert Usey 2018
+// Description : Implementation large integer multiplication
+//               using bruteForce and karatsuba algorithms
 //============================================================================
 
 #include <iostream>
 #include <vector>
 using namespace std;
 
-class Multiplier {
+// MultAlgos CLASS HEADER TO DEFINE THE ALGORITHMS TO BE IMPLEMENTED
+// + HELPER METHODS FOR VECTOR ADDITION AND SUBTRACTION
+class MultAlgos {
 public:
-    Multiplier();
-    virtual ~Multiplier();
+	MultAlgos();
+	virtual ~MultAlgos();
 
-    vector<int> bruteForceMultiply(const vector<int>& a, const vector<int>& b);
-    vector<int> karatsubaMultiply(const vector<int>& a, const vector<int>& b);
-    string toString(const vector<int>& inp);
+	vector<int> bruteForce(const vector<int>& a, const vector<int>& b);
+	vector<int> karatsuba(const vector<int>& a, const vector<int>& b);
+	string print(const vector<int>& vec);
 
 private:
-    vector<int> addVectors(const vector<int>& a, const vector<int>& b);
-    vector<int> subtractVectors(const vector<int>& a, const vector<int>& b);
+	vector<int> addVec(const vector<int>& a, const vector<int>& b);
+	vector<int> subVec(const vector<int>& a, const vector<int>& b);
 
 };
-
-Multiplier::Multiplier() {
+//CONSTRUCTOR/DESCTRUCOR(NOT USED)
+MultAlgos::MultAlgos() {
 }
-Multiplier::~Multiplier() {
-}
-
-
-vector<int> readNumber() {
-    vector<int> num;
-    char c;
-    int temp = 0;
-    int diff = 0;
-
-    // get big number
-    // ignores any non-integer input
-    while(cin.get(c)) {
-        // once we reach the end of the line
-        // we need to stop trying to read input
-        if(c == '\n' || c == '\0') {
-            break;
-        }
-
-        // converts the char into the correct int
-        temp = c - '0';
-
-        // checking to make sure the character is actually a digit
-        // if so, add it to the vector
-        if(temp < 10 && temp >= 0) {
-            num.insert(num.begin(), temp);
-        }
-
-    }
-
-    return num;
+MultAlgos::~MultAlgos() {
 }
 
-void prettifyNumbers(vector<int>& a, vector<int>& b) {
-    int sizeDiff = 0;
+//READ A VECTOR OF INTEGERS FROM CIN
+vector<int> readVecDigits() {
+	vector<int> number;
+	char chr;
+	int tempChr = 0;
+	//READ CHARS FROM CIN
+	while (cin.get(chr)) {
+		//STOP AFTER NEWLINE OR STR TERMINATOR
+		if (chr == '\n' || chr == '\0') {
+			break;
+		}
+		// CONVERT CHAR TO INT
+		tempChr = chr - '0';
 
-    // Make sure the vectors are not empty.
-    // If a number is empty, make it 0.
-    if(a.size() == 0) {
-        a.push_back(0);
-    }
-    if(b.size() == 0) {
-        b.push_back(0);
-    }
+		//DIGIT SANITATION, ADD IT TO NUMBE VEC IF IS DIGIT 0-9
+		if (tempChr < 10 && tempChr >= 0) {
+			number.insert(number.begin(), tempChr);
+		}
+	}
+	return number;
+}
+//APPENDS TRAILING ZEROS TO THE RESPECTIVE VECTOR WITH LESS DIGITS
+void equalizeNumbers(vector<int>& a, vector<int>& b) {
+	int sizeDiff = 0;
 
-    // Ensure the numbers are of equal length
-    // by adding 0s to the end of the shorter one
-    // first, calculate the size sizeDifference
-    sizeDiff = a.size() - b.size();
+	//NEED TO MAKE A. B EQUAL LENGTH BY APPENDING ZEROS RESPECTIVELY
+	//TO THE SMALLER NUMBER
 
-    // a is bigger, so insert the appropriate number of 0s at the end
-    if(sizeDiff > 0) {
-        b.insert(b.end(), sizeDiff, 0);
-    }
-    // b is bigger, so insert the appropriate number of 0s at the end
-    // note the absolute value
-    else if(sizeDiff < 0) {
-        a.insert(a.end(), abs(sizeDiff), 0);
-    }
+	//FIND DIFFERENCE IN SIZE
+	sizeDiff = a.size() - b.size();
+
+	// IF A IS BIGGER INSERT SIZEDIFF AMOUNT OF ZEROS INTO B
+	if (sizeDiff > 0) {
+		b.insert(b.end(), sizeDiff, 0);
+	}
+	// IF B IS BIGGER INSERT SIZEDIFF AMOUNT OF ZEROS INTO A
+	//MUST USE ABSOLUTE VALUE BECAUSE SIZEDIFF WILL BE NEGATIVE IN THIS CASE
+	else if (sizeDiff < 0) {
+		a.insert(a.end(), abs(sizeDiff), 0);
+	}
 }
 
-// implements the brute force algorithm for multiplication that
-// you learned in third grade. uses the partial sums method.
-// O(sizeA * size B) => O(n^2)
-vector<int> Multiplier::bruteForceMultiply(const vector<int>& a, const vector<int>& b) {
+//IMPL OF BURE FORCE MULT
+vector<int> MultAlgos::bruteForce(const vector<int>& a, const vector<int>& b) {
 
-    int mult = 0;
-    int ones = 0;
-    int carry = 0;
-    int numZeros = 0;
-    int sizeA = static_cast<int>(a.size());
-    int sizeB = static_cast<int>(b.size());
-    vector<int> result;
-    vector<int> temp;
+	int mult = 0;
+	int digit = 0;
+	int carry = 0;
+	int numZeros = 0;
+	int sizeA = static_cast<int>(a.size());
+	int sizeB = static_cast<int>(b.size());
+	vector<int> result;
+	vector<int> temp;
 
+	//OUTER LOOP ITERATE OVER A
+	for (int i = 0; i < sizeA; ++i) {
+		carry = 0;
+		//INNER LOOP ITERATE OVER B
+		for (int j = 0; j < sizeB; ++j) {
+			//CLEAR THE TEMP VECTOR EACH INNER FOR ITERATION
+			temp.clear();
 
-    for(int i = 0; i < sizeA; ++i) { // traverse a
-        carry = 0;
+			//INSERT TRAILING ZEROS INTOA TEMP VEC BASED ON HOW FAR INTO
+			//THE ITERATION WE ARE
+			//STORE IN A TEMP VECTOR
+			numZeros = i + j;
+			temp.insert(temp.end(), numZeros, 0);
 
-        for(int j = 0; j < sizeB; ++j) { // traverse b
-            // erase the temp vector
-            temp.clear();
+			//MULTIPLY THE CORRESPONDING DIGITS
+			//CACLULATE THE CARRY AND NUMBER OF ZEROS TO INSERT
+			//INSERT THE MULTPLIED ELEMENTS MOD 10
+			mult = a[i] * b[j] + carry;
+			carry = mult / 10;
+			digit = mult % 10;
+			temp.push_back(digit);
 
-            // insert the appropriate number of zeros into temp
-            // one for every digit we are away from the end
-            // of each number
-            numZeros = i + j;
-            temp.insert(temp.end(), numZeros, 0);
+			//ADD THE PARTIAL RESULT FOR THIS ITERATION
+			result = addVec(result, temp);
+		}
 
-            // get the next digit for our partial sum
-            mult = a[i] * b[j] + carry;
-            carry = mult / 10;
-            ones = mult % 10;
-            temp.push_back(ones);
-
-            // add the partial sum to our total so far
-            result = addVectors(result, temp);
-        } // inner for
-
-        // handle overflow
-        if(carry != 0) {
-            temp.clear();
-            temp.insert(temp.end(), (i + sizeB), 0);
-            temp.push_back(carry);
-            result = addVectors(result, temp);
-        }
-    } // outer for
-
-
-    return result;
+		// IF WE HAVE LEFTOVER CARRY
+		// ADD ZEROS AND ADD FINAL PARTIAL RESULT
+		if (carry != 0) {
+			temp.clear();
+			temp.insert(temp.end(), (i + sizeB), 0);
+			temp.push_back(carry);
+			result = addVec(result, temp);
+		}
+	}
+	return result;
 }
 
-// implements the Karatsuba multiplication algorithm
-// O(n^lg(3)) ~ O(n^1.585)
-// to multiply two 2-digit numbers, the traditional algorithm requires 4 multiplications.
-// this algorithm requires three multiplications, plus some addition operations.
-// when applied recursively, we reduce the number of total multiplications and
-// have asymptotically faster performance.
-vector<int> Multiplier::karatsubaMultiply(const vector<int>& a, const vector<int>& b) {
-    vector<int> result;
-    vector<int> temp;
-    vector<int> a0;
-    vector<int> a1;
-    vector<int> b0;
-    vector<int> b1;
-    vector<int> c0;
-    vector<int> c1;
-    vector<int> c2;
-    int m = 0;
+//KARATSUBA MULT IMPL
+vector<int> MultAlgos::karatsuba(const vector<int>& a, const vector<int>& b) {
+	vector<int> result;
+	vector<int> temp;
+	vector<int> msdA;
+	vector<int> lsdA;
+	vector<int> msdB;
+	vector<int> lsdB;
+	vector<int> c0;
+	vector<int> c1;
+	vector<int> c2;
+	int middle = 0;
 
-    if(a.size() == 1 || b.size() == 1) {
-        // base case can use brute force method for single digits
-        result = bruteForceMultiply(a, b);
-    }
-    else {
-        m = max(a.size(), b.size()) / 2; // floor because int
+	if (a.size() == 1 || b.size() == 1) {
+		//BASECASE MULTIPLY THE SINGLE DIGITS OF A,B USING BRUTEFORCE
+		//COULD NOT FIGURE OUT TO DO A SINGLE MULT WITHOUT KNOWING WHAT POWER OF 10 WE WERE ON
+		result = bruteForce(a, b);
 
-        // copy inputs into two different vectors each
-        // a = a0*10^m + a1
-        // a1 = least significant digits of a = beginning of vector
-        // a0 = most significant digits of a = end of vector
-        for(int i = 0; i < a.size(); ++i) {
-            if(i >= m) {
-                a0.push_back(a[i]);
-                //b0.push_back(b[i]);
-            }
-            else {
-                a1.push_back(a[i]);
-                //b1.push_back(b[i]);
-            }
-        }
+	} else {
+		middle = max(a.size(), b.size()) / 2; // floor because int
 
-        for(int j = 0; j < b.size(); ++ j) {
-            if(j >= m) {
-                b0.push_back(b[j]);
-            }
-            else {
-                b1.push_back(b[j]);
-            }
-        }
+		//DIGITS LEFT OF MIDDLE ARE MOST SIGNIFICANT
+		//DIGITS RIGHT OF MIDDLE ARE LEAST SIGNIFICANT
+		for (int i = 0; i < a.size(); ++i) {
+			if (i >= middle) {
+				msdA.push_back(a[i]);
+			} else {
+				lsdA.push_back(a[i]);
+			}
+		}
+		//DIGITS LEFT OF MIDDLE ARE MOST SIGNIFICANT
+		//DIGITS RIGHT OF MIDDLE ARE LEAST SIGNIFICANT
+		for (int j = 0; j < b.size(); ++j) {
+			if (j >= middle) {
+				msdB.push_back(b[j]);
+			} else {
+				lsdB.push_back(b[j]);
+			}
+		}
+		//=================================
+		//BEGIN DIVIDE AND CONQUER FOR MULT
+		//=================================
 
-        // recursive calls on each half of the input
-        // notice the 3 multiplications
-        c2 = karatsubaMultiply(a1, b1);
-        c1 = karatsubaMultiply(a0, b0);
+		//RECURSE ON BOTH HALVES OF BOTH INPUTS
+		c2 = karatsuba(lsdA, lsdB);
+		c1 = karatsuba(msdA, msdB);
+		//RECURSIVELY FIND CARRY OF ADDING LEAST/MOST SIG DIGITS OF EACH INPUT
+		c0 = karatsuba(addVec(lsdA, msdA), addVec(lsdB, msdB));
 
-        c0 = karatsubaMultiply(addVectors(a1, a0), addVectors(b1, b0));
+		//ADD CARRY DIGITS FROM ONE AND TWO TO TEMP VECTOR
+		//INSERT CORRECT ZEROS INTO C0
+		temp = addVec(c2, c1);
+		c0 = subVec(c0, temp);
+		c0.insert(c0.begin(), middle, 0);
+		//ADD PARTIAL RESULT OF MULT 1 TO RESULT
+		result = addVec(c0, result);
 
-        // middle part
-        // handle the carry
-        // add the correct number of 0s
-        temp = addVectors(c2, c1);
-        c0 = subtractVectors(c0, temp);
-        c0.insert(c0.begin(), m, 0);
-        result = addVectors(c0, result);
+		//ADD MOST SIGNIFICANT DIGITS TO RESULT
+		c1.insert(c1.begin(), 2 * middle, 0);
+		result = addVec(c1, result);
 
-        // most significant digits = c1
-        // add the correct number of 0s
-        c1.insert(c1.begin(), 2*m, 0);
-        result = addVectors(c1, result);
-
-        // least significant digits = c2
-        result = addVectors(c2, result);
-    }
-
-    return result;
+		//ADD LEAST SIGNIFICANT DIGITS TO RESULT
+		result = addVec(c2, result);
+	}
+	return result;
 }
 
+//ADD VECTORS BY DIGIT. WILL AUTO EQUALIZE LENGTH IF NOT EQUAL
+vector<int> MultAlgos::addVec(const vector<int>& a, const vector<int>& b) {
+	int sum = 0;
+	int carry = 0;
+	int digit = 0;
+	int sizeDiff = 0;
+	int sizeA = static_cast<int>(a.size());
+	int sizeB = static_cast<int>(b.size());
+	vector<int> result;
 
+	sizeDiff = sizeA - sizeB;
 
-// adds vectors digit-by-digit
-// they do not need to be the same length
-vector<int> Multiplier::addVectors(const vector<int>& a, const vector<int>& b) {
-    int sum = 0;
-    int carry = 0;
-    int ones = 0;
-    int sizeDiff = 0;
-    int sizeA = static_cast<int>(a.size());
-    int sizeB = static_cast<int>(b.size());
-    vector<int> result;
+	//ADD THE DIGITS UP UNTIL THERE SIZES ARE NO LONGER EQUAL
+	for (int i = 0; i < min(sizeA, sizeB); ++i) {
+		sum = a[i] + b[i] + carry;
+		carry = sum / 10;
+		digit = sum % 10;
+		result.push_back(digit);
+	}
 
-    sizeDiff = sizeA - sizeB;
+	// IF A IS BIGGER INSERT SIZEDIFF AMOUNT OF ZEROS INTO B STARTING AT THE POINT THEY ARE NOT EQUAL LENGTH
+	if (sizeDiff > 0) {
+		//IF A LARGER INSERT ZEROS STARTING AT END INDEX OF B (sizeB)
+		for (int i = sizeB; i < sizeA; ++i) {
+			sum = a[i] + carry;
+			carry = sum / 10;
+			digit = sum % 10;
+			result.push_back(digit);
+		}
 
-    // First, add all of the digits where they both exist
-    for(int i = 0; i < min(sizeA, sizeB); ++i) {
-        sum = a[i] + b[i] + carry;
-        carry = sum / 10;
-        ones = sum % 10;
-        result.push_back(ones);
-    }
-
-    // A has more digits
-    if(sizeDiff > 0) {
-        // first digit of A not in B is i = sizeB
-        for(int i = sizeB; i < sizeA; ++i) {
-            sum = a[i] + carry;
-            carry = sum / 10;
-            ones = sum % 10;
-            result.push_back(ones);
-        }
-
-    }
-    // B has more digits
-    else if(sizeDiff < 0) {
-        // first digit of B not in A is i = sizeA
-        for(int i = sizeA; i < sizeB; ++i) {
-            sum = b[i] + carry;
-            carry = sum / 10;
-            ones = sum % 10;
-            result.push_back(ones);
-        }
-    }
-
-    // regardless of which is bigger,
-    // handle possible overflow from carry
-    if (carry != 0) {
-         result.push_back(carry);
-    }
-
-    return result;
+	}
+	// IF B IS BIGGER INSERT SIZEDIFF AMOUNT OF ZEROS INTO A STARTING AT THE POINT THEY ARE NOT EQUAL LENGTH
+	else if (sizeDiff < 0) {
+		//IF B LARGER INSERT ZEROS STARTING AT END INDEX OF A (sizeA)
+		for (int i = sizeA; i < sizeB; ++i) {
+			sum = b[i] + carry;
+			carry = sum / 10;
+			digit = sum % 10;
+			result.push_back(digit);
+		}
+	}
+	//HANDLE POSSIBLE CARRY OVERFLOW
+	if (carry != 0) {
+		result.push_back(carry);
+	}
+	return result;
 }
 
-// subtracts two vectors digit-by-digit
-// they do not need to be the same length, but a should >= b
-vector<int> Multiplier::subtractVectors(const vector<int>& a, const vector<int>& b) {
-    int diff = 0;
-    int carry = 0;
-    int sizeDiff = 0;
-    int sizeA = static_cast<int>(a.size());
-    int sizeB = static_cast<int>(b.size());
-    bool errorFlag = false;
-    vector<int> result;
+//SUBTRACT VECTORS DIGIT BY DIGIT
+vector<int> MultAlgos::subVec(const vector<int>& a, const vector<int>& b) {
+	int difference = 0;
+	int carry = 0;
+	int sizeDiff = 0;
+	int sizeA = static_cast<int>(a.size());
+	int sizeB = static_cast<int>(b.size());
+	vector<int> result;
 
-    sizeDiff = sizeA - sizeB;
+	sizeDiff = sizeA - sizeB;
 
-    // First, add all of the digits where they both exist
-    for(int i = 0; i < min(sizeA, sizeB); ++i) {
-        diff = a[i] - b[i] - carry;
-        if(diff < 0) {
-            diff += 10;
-            carry = 1;
-        }
-        else {
-            carry = 0;
-        }
-        result.push_back(diff);
-    }
+	//SUBTRACT DIGITS UNTIL THEYRE LENGTHS ARE NO LONGER EQUAL
+	for (int i = 0; i < min(sizeA, sizeB); ++i) {
+		difference = a[i] - b[i] - carry;
+		if (difference < 0) {
+			difference += 10;
+			carry = 1;
+		} else {
+			carry = 0;
+		}
+		result.push_back(difference);
+	}
 
-    // A has more digits
-    if(sizeDiff > 0) {
-        // first digit of A not in B is i = sizeB
-        for(int i = sizeB; i < sizeA; ++i) {
-            diff = a[i] - carry;
-            if(diff < 0) {
-                diff += 10;
-                carry = 1;
-            }
-            else {
-                carry = 0;
-            }
-            result.push_back(diff);
-
-        }
-
-    }
-    // B has more digits
-    // All the extra digits better be 0, otherwise B > A
-    else if(sizeDiff < 0) {
-
-        // first digit of B not in A is i = sizeA
-        for(int i = sizeA; i < sizeB; ++i) {
-            // if we get to a digit that isn't zero,
-            // B was bigger, so we flag an error
-            if (b[i] != 0) {
-                errorFlag = true;
-                break;
-            }
-        }
-
-    }
-
-    // if we flagged an error, return 0.
-    if(errorFlag) {
-        result.clear();
-        result.push_back(0);
-    }
-
-    return result;
+	//A HAS MORE DIGITS
+	if (sizeDiff > 0) {
+		//IF A LARGER INSERT ZEROS STARTING AT END INDEX OF B (sizeB)
+		for (int i = sizeB; i < sizeA; ++i) {
+			difference = a[i] - carry;
+			if (difference < 0) {
+				difference += 10;
+				carry = 1;
+			} else {
+				carry = 0;
+			}
+			result.push_back(difference);
+		}
+	}
+	// B HAS MORE DIGITS
+	// IF THE EXTRA DIGITS ARE NOT ZEROS, B>A WE CANNOT CONTINUE
+	else if (sizeDiff < 0) {
+		for (int i = sizeA; i < sizeB; ++i) {
+			//IF WE FIND A TRAILING ZERO IN B, A,B ARE NOT EQUAL AND CANNOT BE SUBTRACTED
+			//SO CLEAR RESULT AND RETURN 0
+			if (b[i] != 0) {
+				result.clear();
+				result.push_back(0);
+				return result;
+			}
+		}
+	}
+	return result;
 }
-
-string Multiplier::toString(const vector<int>& inp) {
-    string ret = "";
-    vector<int>::const_reverse_iterator it;
-
-    for(it = inp.rbegin(); it != inp.rend(); ++it) {
-        ret += to_string(*it);
-    }
-
-    return ret;
+//PRINTS A CALCULATE LARGE INT MULT RESULT USING REVERSE ORDER ITERATION
+string MultAlgos::print(const vector<int>& vec) {
+	string ret = "";
+	//CREATE A REVERSE ITERATOR BECAUSE OUR RESULT IS BACKWARDS
+	vector<int>::const_reverse_iterator it;
+	//STARTING FROM THE END APPEND EACH DIGIT TO OUR RETURN STRING UNTIL THERE ARE NO MORE
+	for (it = vec.rbegin(); it != vec.rend(); ++it) {
+		ret += to_string(*it);
+	}
+	return ret;
 }
 int main(int argc, char *argv[]) {
-	vector<int> a = readNumber();
-	vector<int> b = readNumber();
-	prettifyNumbers(a, b);
-
-	Multiplier multiplier = Multiplier();
-
-	cout << "Brute Force: "
-			<< multiplier.toString(multiplier.bruteForceMultiply(a, b)) << endl;
-	cout << "Karatsuba: "
-			<< multiplier.toString(multiplier.karatsubaMultiply(a, b)) << endl;
+	//READ TWO NUMBERS FROM CIN USING OUR METHOD
+	vector<int> a = readVecDigits();
+	vector<int> b = readVecDigits();
+	//QUALIZE THE LENGTH OF BOTH VECTORS
+	equalizeNumbers(a, b);
+	//STACK ALLOC OF MULTALGOS CLASS.
+	MultAlgos multiplier = MultAlgos();
+	//OUTPUT OUR RESULTS
+	cout << "Brute Force: " << multiplier.print(multiplier.bruteForce(a, b))
+			<< endl;
+	cout << "Karatsuba: " << multiplier.print(multiplier.karatsuba(a, b))
+			<< endl;
 }
